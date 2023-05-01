089593cb0879653899cb0df55622c03550d790ab
Meet <meet.kmphitech@gmail.com>
2023-05-01 17:29:11 +0530

    changes

--- app/src/main/java/com/example/retrofitusingnetworking/collapsingtoolbar/ToolBarActivity.kt
+++ app/src/main/java/com/example/retrofitusingnetworking/collapsingtoolbar/ToolBarActivity.kt
@@ -1,12 +1,44 @@
 package com.example.retrofitusingnetworking.collapsingtoolbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.retrofitusingnetworking.R
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
+import com.google.android.material.appbar.CollapsingToolbarLayout
+
 
 class ToolBarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tool_bar)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        val appBarLayout = findViewById<AppBarLayout>(R.id.app_bar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setHomeButtonEnabled(true)
+        val collapsingToolbarLayout = findViewById<CollapsingToolbarLayout>(R.id.collapsingToolbar)
+
+
+        appBarLayout.addOnOffsetChangedListener(object : OnOffsetChangedListener {
+            var isShow = false
+            var scrollRange = -1
+            override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.totalScrollRange
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.title = "CodingWithMe"
+                    isShow = false
+                } else {
+                    collapsingToolbarLayout.title = ""
+                    isShow = true
+                }
+            }
+        })
+
+
     }
 }
